0ff18c4b239bf0a5e49bef366c6790aac817ba57
Brendon Xie <brendontyxie@gmail.com>
2026-02-03 22:34:59 -0500

    wrote countNoDoubleLetterWords and tested

--- 2026-02-03-recursion/Backtracking.java
+++ 2026-02-03-recursion/Backtracking.java
@@ -9,15 +9,37 @@ public class Backtracking{
     *        is only one unique letter provided, but that is an edge case.
     *Repetition allowed except when letters are adjacent e.g. "abababa" is allowed.
     */
-  public static long countNoDoubleLetterWords(int length,String word, String letters){
-    //Hint: not a wrapper method, but you must call it starting with "" as your word
-    // e.g. countNoDoubleLetterWords(5,"","abc")
-      return countNoDoubleLetterWords()
+  public static long countNoDoubleLetterWords(int length, String word, String letters) {
 
+    if (word.length() == length) {
+        return 1;
+    }
+    long total = 0;
+
+    for (int i = 0; i < letters.length(); i++) {
+      char c = letters.charAt(i);
+
+      if (word.equals("") || word.charAt(word.length() - 1) != c) {
+          total += countNoDoubleLetterWords(length, word + c, letters);
+      }
+    }
+
+    return total;
   }
+      
 
+
+/* 
    public static boolean groupSum(int start, int[] nums, int target) {
 
 
    }
+*/
+
+  public static void main (String[] args){
+    System.out.println(countNoDoubleLetterWords(2, "", "abc"));
+    System.out.println(countNoDoubleLetterWords(3, "", "abcdefghijklmnopqrstuvwxyz"));
+
+
+  }
 }
