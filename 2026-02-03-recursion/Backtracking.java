1d87d3301757276fb11dc58558c5e96e91558479
Brendon Xie <brendontyxie@gmail.com>
2026-02-08 13:21:56 -0500

    wrote and tsted groupSum5

--- 2026-02-03-recursion/Backtracking.java
+++ 2026-02-03-recursion/Backtracking.java
@@ -139,6 +139,22 @@ public class Backtracking{
     helper53(start + 1, nums, sum1, sum2 + value);
   }
 
+  public static boolean groupSum5(int start, int[] nums, int target){
+    if (start >= nums.length){
+      return target == 0;
+    }
+
+    if (nums[start] % 5 == 0){
+      if (start < nums.length - 1 && nums[start+1] == 1){
+        return groupSum5(start + 2, nums, target - nums[start]);
+      }
+      return groupSum5(start + 1, nums, target - nums[start]);
+    }
+
+    return groupSum5(start + 1, nums, target - nums[start])||
+    groupSum5(start + 1, nums, target);
+  }
+
 
 
   public static void main (String[] args){
