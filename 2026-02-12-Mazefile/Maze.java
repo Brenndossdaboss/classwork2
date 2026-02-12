5f9ce705f7f28de7c4834b4c996c4077c56be7b8
Brendon Xie <brendontyxie@gmail.com>
2026-02-12 11:47:33 -0500

    started getMaze

--- 2026-02-12-Mazefile/Maze.java
+++ 2026-02-12-Mazefile/Maze.java
@@ -1,4 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Maze{
 
-  
+  public static char[][] getMaze(String filename) throws FileNotFoundException{
+    Scanner sc = new Scanner(new File(filename));
+    String mazeString = "";
+    char[][] maze = new char[][];
+
+
+
+  }
 }
