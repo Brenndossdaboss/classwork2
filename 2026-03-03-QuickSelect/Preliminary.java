428556b159ac17a5d002f24bd49a5bff4ab6eaf5
Brendon Xie <brendontyxie@gmail.com>
2026-03-03 23:20:43 -0500

    wrote partition and swap helper

--- 2026-03-03-QuickSelect/Preliminary.java
+++ 2026-03-03-QuickSelect/Preliminary.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Preliminary{
+public class Preliminary {
 
   /**Modify the array as per the partition algorithm discussed in class
   *@param data is the array to partition
@@ -18,12 +18,44 @@ public class Preliminary{
   * the values at indices < lo are not changed
   * the values at indices > hi are not changed
   */
-  public static int partition( int [] data, int lo, int hi){
-
+  public static int partition(int[] data, int lo, int hi) {
     Random r = new Random();
-    int p = r.nextInt(lo, hi + 1);
 
+    int pivotIndex = r.nextInt(lo, hi + 1);
+    int pivot = data[pivotIndex];
+    swap(data, pivotIndex, lo);
 
-    return 0;
+    int left = lo + 1;
+    int right = hi;
+
+    boolean equalGoesLeft = true;
+
+    while (left <= right) {
+      if (data[left] < pivot) {
+        left++;
+      } else if (data[right] > pivot) {
+        right--;
+      } else if (data[left] == pivot && data[right] == pivot) {
+        swap(data, left, right);
+        left++;
+        right--;
+      } else if (data[left] > pivot || (data[left] == pivot && !equalGoesLeft)) {
+        swap(data, left, right);
+        right--;
+        if (data[left] == pivot) equalGoesLeft = !equalGoesLeft;
+      } else {
+        swap(data, left, right);
+        left++;
+        if (data[right] == pivot) equalGoesLeft = !equalGoesLeft;
+      }
+    }
+    swap(data, lo, right);
+    return right;
+  }
+
+  private static void swap(int[] data, int i, int j) {
+    int temp = data[i];
+    data[i] = data[j];
+    data[j] = temp;
   }
 }
